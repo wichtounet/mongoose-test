9eb1f4a47bfedeeed4a376b86e9880fdd7120c40
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-26 08:00:50 +0100

    Use the mongoose-cpp fork

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,19 @@
 #include <mongoose/Server.h>
 #include <mongoose/WebController.h>
 
-//Unfortunately these lines are mandatory because of poor coding of the binding
-using namespace std;
-using namespace Mongoose;
-
-struct hello_controller : public WebController {
-    void hello(Request& request, StreamResponse& response){
+struct hello_controller : public Mongoose::WebController {
+    void hello(Mongoose::Request& request, Mongoose::StreamResponse& response){
         response << "Hello " << htmlEntities(request.get("name", "... what's your name ?")) << std::endl;
     }
 
-    void display(Request& /*request*/, StreamResponse& response){
+    void display(Mongoose::Request& /*request*/, Mongoose::StreamResponse& response){
         response << "<html><head><title>Test</title></head><body><h1>Hello you!</h1><div>This is a web page generated in C++ and I'm proud of it :P</div></html>" << std::endl;
     }
 
     //This will be called automatically
     void setup(){
-        addRoute("GET", "/hello", hello_controller, hello);
-        addRoute("GET", "/display", hello_controller, display);
+        addRoute<hello_controller>("GET", "/hello", &hello_controller::hello);
+        addRoute<hello_controller>("GET", "/display", &hello_controller::display);
     }
 };
 
@@ -31,7 +27,7 @@ int main(){
     hello_controller controller;
 
     // Run the server with our controller
-    Server server(8080);
+    Mongoose::Server server(8080);
     server.registerController(&controller);
 
     // Start the server and wait forever
